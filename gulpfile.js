d444cd1684f1289362317ada87b28bb9c4d6ca79
Starlone <starlone.passos@gmail.com>
2016-09-26 23:29:52 +0000

    atualizando projeto apartir do gerador

--- gulpfile.js
+++ gulpfile.js
@@ -30,7 +30,7 @@ function watch(done) {
     'bower.json'
   ], gulp.parallel('inject'));
 
-  gulp.watch(conf.path.src('app/**/*.html'), reloadBrowserSync);
+  gulp.watch(conf.path.src('app/**/*.html'), gulp.series('partials', reloadBrowserSync));
   gulp.watch([
     conf.path.src('**/*.css')
   ], gulp.series('styles'));
