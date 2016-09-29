224fa0162340dc142fea2f904b091b9a115a5282
Starlone <starlone.passos@gmail.com>
2016-09-29 00:55:42 +0000

    Dividindo a tela em 3 colunas. Primeira para os objetos do projeto, segunda para visualizacao do jogo e terceira para as propriedades de um objeto especifico

--- src/app/main.js
+++ src/app/main.js
@@ -1,8 +1,17 @@
+/* global se:true */
+/* eslint no-undef: 'error' */
+
 angular
   .module('app')
   .component('app', {
     templateUrl: 'app/main.html',
-    controller: function () {
-      this.hello = 'Hello World!';
+    controller: function ($scope) {
+      var vm = $scope;
+      vm.game = new se.StarEngine('canvas');
+
+      var scene = new se.Scene(vm.game, new se.GradientRenderer('#004CB3', '#8ED6FF'));
+      vm.game.addScene(scene);
+
+      vm.game.run();
     }
   });
