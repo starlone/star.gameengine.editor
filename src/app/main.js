7b963adfea78c8c9e58ba93c42812ebc5ae4fe3d
Starlone <starlone.passos@gmail.com>
2016-09-29 13:41:36 -0300

    Listando a scena atual com seus objetos. Ao clicar em um objeto é descrito seu nome e posicao na view properties

--- src/app/main.js
+++ src/app/main.js
@@ -1,6 +1,7 @@
 /* global se:true */
 /* eslint no-undef: 'error' */
 
+var x;
 angular
   .module('app')
   .component('app', {
@@ -9,9 +10,22 @@ angular
       var vm = $scope;
       vm.game = new se.StarEngine('canvas');
 
-      var scene = new se.Scene(vm.game, new se.GradientRenderer('#004CB3', '#8ED6FF'));
-      vm.game.addScene(scene);
+      vm.scene = new se.Scene(vm.game, new se.GradientRenderer('#004CB3', '#8ED6FF'));
+      vm.game.addScene(vm.scene);
+
+      var grass = se.factory.rect({name: 'Rect', x: 1000, y: 300, w: 3800, h: 30, fillColor: 'green', rigidopts: {isStatic: true}});
+      vm.scene.add(grass);
 
       vm.game.run();
+
+      vm.showProperties = function (obj) {
+        var pos = obj.transform.position;
+        vm.prop = {
+          name: obj.name,
+          x: pos.x,
+          y: pos.y
+        };
+        vm.prop.obj = obj;
+      };
     }
   });
