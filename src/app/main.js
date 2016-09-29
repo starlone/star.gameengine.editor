f83379787ac3a05d5e8fc73d4715352893074eb7
Starlone <starlone.passos@gmail.com>
2016-09-29 23:23:03 +0000

    Add botao de new object

--- src/app/main.js
+++ src/app/main.js
@@ -12,10 +12,16 @@ angular
       vm.scene = new se.Scene(vm.game, new se.GradientRenderer('#004CB3', '#8ED6FF'));
       vm.game.addScene(vm.scene);
 
-      var grass = se.factory.rect({name: 'Rect', x: 1000, y: 300, w: 3800, h: 30, fillColor: 'green', rigidopts: {isStatic: true}});
-      vm.scene.add(grass);
+      var ground = se.factory.rect({name: 'Ground', x: 1000, y: 300, w: 3800, h: 30, fillColor: 'green', rigidopts: {isStatic: true}});
+      vm.scene.add(ground);
 
       vm.game.run();
+
+      vm.newObject = function () {
+        var obj = se.factory.rect({name: 'Rect', x: 0, y: 0, w: 58, h: 50, fillColor: '#b0bec5'});
+        vm.scene.add(obj);
+        vm.showProperties(obj);
+      };
 
       vm.showProperties = function (obj) {
         var pos = obj.transform.position;
