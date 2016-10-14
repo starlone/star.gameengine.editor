a3969a4c0ecbe8503723423536443b73d275e089
Starlone <starlone.passos@gmail.com>
2016-10-14 13:25:11 -0300

    removendo se-panel não necessario por enquanto

--- src/app/game.js
+++ src/app/game.js
@@ -4,7 +4,7 @@
 angular
   .module('app')
   .factory('$game', function () {
-    var game = new se.StarEngine('canvas');
+    var game = new se.StarEngine('gameview');
     var scene = new se.Scene(game, new se.GradientRenderer('#004CB3', '#8ED6FF'));
     game.addScene(scene);
     return game;
--- src/app/panel.js
+++
@@ -1,9 +0,0 @@
-angular
-  .module('app')
-  .component('sePanel', {
-    transclude: true,
-    templateUrl: 'app/panel.html',
-    bindings: {
-      title: '@'
-    }
-  });
