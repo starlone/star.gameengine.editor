1713283d245ddb65f09e34325035cc7bdc34baa5
Starlone <starlone.passos@gmail.com>
2016-12-06 16:34:24 -0200

    Adicionando botão para iniciar e parar physics

--- src/app/gameview.js
+++ src/app/gameview.js
@@ -5,6 +5,18 @@ angular
     controller: function ($scope, $managegame) {
       var vm = $scope;
       vm.game = $managegame.game;
+      vm.game.runner.enabled = false;
       vm.game.run();
+      vm.playmsg = 'Play physics';
+
+      vm.play = function () {
+        // Toggle
+        vm.game.runner.enabled = !vm.game.runner.enabled;
+        if (vm.game.runner.enabled) {
+          vm.playmsg = 'Stop physics';
+        } else {
+          vm.playmsg = 'Play physics';
+        }
+      };
     }
   });
