2195646c0abd3c3c07b5073c21d1dba5b3fb0161
Starlone <starlone.passos@planejamento.gov.br>
2017-01-02 23:47:38 -0200

    movendo objeto selecionado ao pressionar teclas de setas

--- src/app/panels/panel_gameview.js
+++ src/app/panels/panel_gameview.js
@@ -7,5 +7,26 @@ angular
       vm.game = $managegame.game;
       vm.game.runner.enabled = false;
       vm.game.run();
+
+      var keydown = function (key) {
+        var x = 0;
+        var y = 0;
+        if (key === 37) { // Left
+          x = -3;
+        } else if (key === 39) { // 'right'
+          x = 3;
+        } else if (key === 38) { // Up
+          y = -3;
+        } else if (key === 40) { // 'Down'
+          y = 3;
+        }
+        $managegame.selected.transform.move(x, y);
+      };
+
+      document.addEventListener('keydown', function (e) {
+        if ($managegame.selected) {
+          keydown(e.keyCode);
+        }
+      });
     }
   });
