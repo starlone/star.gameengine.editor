79e5c873251219c3ce75e203db9f009f5b2bf3d2
Starlone <starlone.passos@gmail.com>
2017-01-12 21:43:36 -0200

    Ao instanciar Scene nao passando game

--- src/app/game.js
+++ src/app/game.js
@@ -5,7 +5,7 @@ angular
   .module('app')
   .factory('$managegame', function () {
     var game = new se.StarEngine('gameview');
-    var scene = new se.Scene(game, new se.GradientRenderer('#004CB3', '#8ED6FF'));
+    var scene = new se.Scene(new se.GradientRenderer('#004CB3', '#8ED6FF'));
     game.addScene(scene);
     var renderOld = null;
     var renderer = new se.MeshRenderer('#ffb74d', '#e65100', 5);
